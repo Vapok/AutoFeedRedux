13d57deba101ef92051472da0e78ab316e455611
Vapok <vapokrocks@gmail.com>
2026-09-16 18:01:21 -0400

    Release v2.0.3: Unified Splash Screen & Telemetry Controls

--- AutoFeedRedux/AutoFeedRedux.cs
+++ AutoFeedRedux/AutoFeedRedux.cs
@@ -24,7 +24,7 @@ namespace AutoFeedRedux
         //Module Constants
         private const string _pluginId = "vapok.mods.AutoFeedRedux";
         private const string _displayName = "AutoFeedRedux";
-        private const string _version = "2.0.2";
+        private const string _version = "2.0.3";
         
         //Interface Properties
         public string PluginId => _pluginId;
